36f801232dd1733f369db4ec3d0d51e3715082ab
Stas <39906128+RomanovStas@users.noreply.github.com>
2018-12-13 11:49:19 +0300

    apdate

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("album");
+
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+
+  slides[slideIndex-1].style.display = "block"; 
+
+}
 
 fetch('http://jsonplaceholder.typicode.com/albums/1/photos').then(function(response) {
 
@@ -27,6 +47,5 @@ fetch('http://jsonplaceholder.typicode.com/albums/1/photos').then(function(respo
 
   });
 }).catch(err => console.log('Error: ${err}'));
-
 
 
